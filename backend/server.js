0cef04b5219fe78136441b7a1ced107a21dde72f
jahmir <altairjohnfbacc@gmail.com>
2021-01-03 15:05:10 +0800

    updates

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('API is running 2...')
+    res.send('API is running ...')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+//link /api/products to productRoutes
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log('Server running on port 5000'))
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
