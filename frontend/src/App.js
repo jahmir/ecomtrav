2fde923c3283d3433ecdeb9ea4c0cab2638553a3
jahmir <altairjohnfbacc@gmail.com>
2021-01-05 21:52:55 +0800

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 
 const App = () => {
@@ -15,9 +16,10 @@ const App = () => {
         <Header />
         <Container>
           <main className='py-3'>
-            <Route exact path='/' component={HomeScreen} />
+            <Route exact path='/login' component={LoginScreen} />
             <Route exact path='/product/:id' component={ProductScreen} />
             <Route exact path='/cart/:id?' component={CartScreen} />
+            <Route exact path='/' component={HomeScreen} />
           </main>
         </Container>
         <Footer />
